dc48eaf65470e9208285788f0f7b7af715cf3211
Jeferson Christofer <jeferson.c.mga@gmail.com>
2019-02-22 16:43:43 -0300

    Deletando uma categoria

--- src/main/java/com/chris/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/chris/cursomc/resources/CategoriaResource.java
@@ -7,6 +7,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.chris.cursomc.domain.Categoria;
+import com.chris.cursomc.dto.CategoriaDTO;
 import com.chris.cursomc.services.CategoriaService;
 
 @RestController
@@ -50,5 +54,12 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
 		srv.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = srv.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
