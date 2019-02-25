f6e0dbc2ba18d6a397972afb4111a8733ecf6fe5
Jeferson Christofer <jeferson.c.mga@gmail.com>
2019-02-25 14:11:32 -0300

    Validação de email repetido na atualização do cliente

--- src/main/java/com/chris/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/chris/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.chris.cursomc.domain.Cliente;
+import com.chris.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
